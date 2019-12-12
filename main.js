471cb6e53432341d6005f40cf83b1de5051eacb1
conrad96 <conrad.mugisha@gmail.com>
2019-12-12 18:41:39 +0300

    added extra code

--- main.js
+++ main.js
@@ -5,12 +5,19 @@ const shell = require('electron').shell
 
 let window, menu;
 app.on('ready', function(){
-    window = new BrowserWindow({});
+    window = new BrowserWindow(
+        {
+            webPreferences: {
+            nodeIntegration: true
+            }
+        });
     window.loadURL(url.format({
         pathname: path.join(__dirname, 'src/index.html'),
         protocol: 'file:',
         slashes: true
     }));
+    //
+    window.webContents.openDevTools();
     //create menu
     menu = Menu.buildFromTemplate([
         {
